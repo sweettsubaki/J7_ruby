31925a7a708ca4214b4fdc7e17480c565bed7906
Célia <73950039+sweettsubaki@users.noreply.github.com>
2021-04-13 20:02:04 +0200

    Update exo_17.rb

--- exo_17.rb
+++ exo_17.rb
@@ -2,17 +2,7 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print ">"
 floor = gets.chomp.to_i
 
-while floor > 25
-    puts "La pyramide ne doit pas dépasser les 25 étages, please try again"
-    print ">"
-floor = gets.chomp.to_i
-end
-
-compt = 1
-dieses = "#"
-
-center_value = floor * 2 -1
-floor.times do
-    dieses += '##'
-    puts dieses.center(center_value)
-end
+upto(floor) do |i|
+    spaces = " " * (floor - i) 
+    dieses = "#" (1*2 - 1)
+puts spaces + dieses + spaced
